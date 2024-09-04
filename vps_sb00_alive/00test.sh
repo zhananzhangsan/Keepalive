9177f7a527d1ae81ecb434d7f8b422966b3ff305
yutian81 <165384851+yutian81@users.noreply.github.com>
2024-09-04 13:43:02 +0800

    Update 00test.sh

--- vps_sb00_alive/00test.sh
+++ vps_sb00_alive/00test.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-#脚本来源：老王 https://github.com/eooce/scripts
 
-SCRIPT_PATH="/root/00test.sh"  # 脚本路径
-
+SCRIPT_PATH="/root/sb00_alive.sh"  # 脚本路径
 export HOST=${HOST:-'s11.serv00.com'}   # serv00服务器或IP
-export TCP1_PORT=${TCP1_PORT:-'1234'}     # 四合一vmess端口
-export TCP2_PORT=$TCP2_PORT:-'5678'}   # 四合一socks5端口
-export UDP_PORT=${UDP_PORT:-'6789'}   # 四合一hy2端口
+export VMESS_PORT=${VMESS_PORT:-'1234'}     # 四合一vmess端口
+export SOCKS_PORT=$SOCKS_PORT:-'5678'}   # 四合一socks5端口
+export HY2_PORT=${HY2_PORT:-'6789'}   # 四合一hy2端口
 export SSH_USER=${SSH_USER:-'abcd'}  # serv00或ct8账号
 export SSH_PASS=${SSH_PASS:-'12345678'}  # serv00或ct8密码
 
 # 最大尝试检测次数
 MAX_ATTEMPTS=5
-
 attempt=0
 
 # 根据对应系统安装依赖
@@ -26,13 +23,32 @@ install_packages() {
     elif [ -f /etc/alpine-release ]; then
         package_manager="apk add"
     else
-        echo -e"${red}不支持的系统架构！${reset}"
+        echo -e "${red}不支持的系统架构！${reset}"
         exit 1
     fi
-    $package_manager sshpass curl netcat-openbsd cron > /dev/null
+    $package_manager sshpass curl netcat-openbsd cron jq > /dev/null
 }
 install_packages
-clear
+
+# 拉取远程 json 文件
+curl -s https://github.yutian81.top/yutian81/Wanju-Nodes/main/serv00-panel3/sb00ssh.json -o sb00ssh.json
+
+# 读取 JSON 文件并遍历每个服务器的配置
+jq -c '.[]' "sb00ssh.json" | while IFS= read -r server; do
+    HOST=$(echo "$server" | jq -r '.HOST')
+    SSH_USER=$(echo "$server" | jq -r '.SSH_USER')
+    SSH_PASS=$(echo "$server" | jq -r '.SSH_PASS')
+    VMESS_PORT=$(echo "$server" | jq -r '.VMESS_PORT')
+    SOCKS_PORT=$(echo "$server" | jq -r '.SOCKS_PORT')
+    HY2_PORT=$(echo "$server" | jq -r '.HY2_PORT')
+    ARGO_DOMAIN=$(echo "$server" | jq -r '.ARGO_DOMAIN')
+    ARGO_AUTH=$(echo "$server" | jq -r '.ARGO_AUTH')
+    NEZHA_SERVER=$(echo "$server" | jq -r '.NEZHA_SERVER')
+    NEZHA_PORT=$(echo "$server" | jq -r '.NEZHA_PORT')
+    NEZHA_KEY=$(echo "$server" | jq -r '.NEZHA_KEY')
+
+# 打印正在处理的服务器信息
+echo "Processing server: $HOST"
 
 # 添加定时任务的函数
 add_cron_job() {
@@ -46,28 +62,28 @@ add_cron_job() {
 add_cron_job
 
 # 检测 TCP 端口是否通畅
-check_tcp_port() {
-  nc -zv $HOST $TCP_PORT &> /dev/null
+check_vmess_port() {
+  nc -zv $HOST $VMESS_PORT &> /dev/null
   return $?
 }
 
-# 连接ssh并执行远程命令
+# 连接并执行远程命令的函数
 run_remote_command() {
-  sshpass -p "$SSH_PASS" ssh -o StrictHostKeyChecking=no $SSH_USER@$HOST \
-    "ps aux | grep $(whoami) | grep -v 'sshd\|bash\|grep' | awk '{print $2}' | xargs -r kill -9 > /dev/null 2>&1 && \
-    cd /home/$SSH_USER/logs && \
-    nohup ./nezha.sh >/dev/null 2>&1 & \
-    nohup ./web run -c config.json >/dev/null 2>&1 & \
-    nohup ./argo.sh >/dev/null 2>&1 &"
+   sshpass -p "$SSH_PASS" ssh -o StrictHostKeyChecking=no "$SSH_USER@$HOST" \
+     "ps aux | grep $(whoami) | grep -v 'sshd\|bash\|grep' | awk '{print \$2}' | xargs -r kill -9 > /dev/null 2>&1 && \
+     cd /home/$SSH_USER/logs && \
+     nohup ./nezha.sh >/dev/null 2>&1 & \
+     nohup ./web run -c config.json >/dev/null 2>&1 & \
+     nohup ./argo.sh >/dev/null 2>&1 &"
 }
 
 # 循环检测
 while [ $attempt -lt $MAX_ATTEMPTS ]; do
-  if check_tcp_port; then
-    echo -e "\e[1;32m程序已运行，TCP 端口 $TCP_PORT 通畅\e[0m\n"
+  if check_vmess_port; then
+    echo -e "\e[1;32m程序已运行，TCP 端口 $VMESS_PORT 通畅\e[0m\n"
     exit 0
   else
-    echo -e "\e[1;33mTCP 端口 $TCP_PORT 不通畅，进程可能不存在，休眠30s后重试\e[0m"
+    echo -e "\e[1;33mTCP 端口 $VMESS_PORT 不通畅，进程可能不存在，休眠30s后重试\e[0m"
     sleep 30
     attempt=$((attempt+1))
   fi
