d7977ef2dc82b2889be49251d9b33ad76ae22717
yutian81 <165384851+yutian81@users.noreply.github.com>
2024-09-05 14:30:06 +0800

    Update sb_serv00_socks.sh

--- sb_serv00_socks.sh
+++ sb_serv00_socks.sh
@@ -265,7 +265,7 @@ run_sb() {
 argodomain=$(get_argodomain)
   if [ -e bot ]; then
     if [[ $ARGO_AUTH =~ ^[A-Z0-9a-z=]{120,250}$ ]]; then
-      args="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token $ARGO_AUTH --hostname $argodomain --url http://localhost:$vmess_port"
+      args="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token $ARGO_AUTH --hostname $argodomain"
     elif [[ $ARGO_AUTH =~ TunnelSecret ]]; then
       args="tunnel --edge-ip-version auto --config tunnel.yml run"
     else
