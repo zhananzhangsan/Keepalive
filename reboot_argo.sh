50110f4c0c99a67dba1278bed5cb9313ae494ca0
yutian81 <165384851+yutian81@users.noreply.github.com>
2024-09-05 19:00:52 +0800

    Update reboot_argo.sh

--- reboot_argo.sh
+++ reboot_argo.sh
@@ -3,13 +3,12 @@
 USERNAME=$(whoami)
 WORKDIR="/home/${USERNAME}/logs"
 chmod 777 "$WORKDIR"
-cd $WORKDIR
 
 # 检查 argo 服务运行状态并启动
 if pgrep -x "bot" >/dev/null; then
    green "ARGO 正在运行"
 else
-   red "singbox 未运行，重启中……"
-   pkill -x "bot" && nohup ./argo.sh >/dev/null 2>&1 & sleep 1
+   red "ARGO 未运行，重启中……"
+   pkill -x "bot" && nohup $WORKDIR/argo.sh >/dev/null 2>&1 & sleep 1
    green "ARGO 已重启"
 fi
