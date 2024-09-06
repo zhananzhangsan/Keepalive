c3346f200a6242ef2fd808d59c9240e4186b1293
yutian81 <165384851+yutian81@users.noreply.github.com>
2024-09-06 13:14:56 +0800

    Update check_sb_cron.sh

--- check_sb_cron.sh
+++ check_sb_cron.sh
@@ -8,32 +8,29 @@ CRON_SB="nohup ${WORKDIR}/web run -c ${WORKDIR}/config.json >/dev/null 2>&1 &"
 CRON_ARGO="nohup ${WORKDIR}/argo.sh >/dev/null 2>&1 &"
 
 echo "检查并添加 crontab 任务"
+# 先清除旧的相关任务
+(crontab -l | grep -v -E "@reboot pkill -kill -u $(whoami)|pgrep -x \"npm\"|pgrep -x \"web\"|pgrep -x \"bot\"") | crontab -
+
+# 判断文件是否存在，并根据情况添加任务
 if [ -e "${WORKDIR}/nezha.sh" ] && [ -e "${WORKDIR}/config.json" ] && [ -e "${WORKDIR}/argo.sh" ]; then
   echo "添加 nezha & singbox & argo 的 crontab 重启任务"
-  (crontab -l | grep -F "@reboot pkill -kill -u "$(whoami)" && ${CRON_NEZHA} ${CRON_SB} ${CRON_ARGO}") || \
-    (crontab -l; echo "@reboot pkill -kill -u "$(whoami)" && ${CRON_NEZHA} ${CRON_SB} ${CRON_ARGO}") | crontab -
-  (crontab -l | grep -F "pgrep -x \"npm\" > /dev/null || ${CRON_NEZHA}") || \
-    (crontab -l; echo "*/10 * * * * pgrep -x \"npm\" > /dev/null || ${CRON_NEZHA}") | crontab -
-  (crontab -l | grep -F "pgrep -x \"web\" > /dev/null || ${CRON_SB}") || \
-    (crontab -l; echo "*/10 * * * * pgrep -x \"web\" > /dev/null || ${CRON_SB}") | crontab -
-  (crontab -l | grep -F "pgrep -x \"bot\" > /dev/null || ${CRON_ARGO}") || \
-    (crontab -l; echo "*/10 * * * * pgrep -x \"bot\" > /dev/null || ${CRON_ARGO}") | crontab -
+  (crontab -l; echo "@reboot pkill -kill -u $(whoami) && ${CRON_NEZHA} ${CRON_SB} ${CRON_ARGO}") | crontab -
+  (crontab -l; echo "*/10 * * * * pgrep -x \"npm\" > /dev/null || ${CRON_NEZHA}") | crontab -
+  (crontab -l; echo "*/10 * * * * pgrep -x \"web\" > /dev/null || ${CRON_SB}") | crontab -
+  (crontab -l; echo "*/10 * * * * pgrep -x \"bot\" > /dev/null || ${CRON_ARGO}") | crontab -
+
 elif [ -e "${WORKDIR}/nezha.sh" ]; then
   echo "添加 nezha 的 crontab 重启任务"
-  (crontab -l | grep -F "@reboot pkill -kill -u "$(whoami)" && ${CRON_NEZHA}") || \
-    (crontab -l; echo "@reboot pkill -kill -u "$(whoami)" && cd ${WORKDIR} && ${CRON_NEZHA}") | crontab -
-  (crontab -l | grep -F "pgrep -x \"npm\" > /dev/null || ${CRON_NEZHA}") || \
-    (crontab -l; echo "*/10 * * * * pgrep -x \"npm\" > /dev/null || ${CRON_NEZHA}") | crontab -
+  (crontab -l; echo "@reboot pkill -kill -u $(whoami) && cd ${WORKDIR} && ${CRON_NEZHA}") | crontab -
+  (crontab -l; echo "*/10 * * * * pgrep -x \"npm\" > /dev/null || ${CRON_NEZHA}") | crontab -
+
 elif [ -e "${WORKDIR}/config.json" ]; then
   echo "添加 singbox 的 crontab 重启任务"
-  (crontab -l | grep -F "@reboot pkill -kill -u "$(whoami)" && ${CRON_SB}") || \
-    (crontab -l; echo "@reboot pkill -kill -u "$(whoami)" && cd ${WORKDIR} && ${CRON_SB}") | crontab -
-  (crontab -l | grep -F "pgrep -x \"web\" > /dev/null || ${CRON_SB}") || \
-    (crontab -l; echo "*/10 * * * * pgrep -x \"web\" > /dev/null || ${CRON_SB}") | crontab -
+  (crontab -l; echo "@reboot pkill -kill -u $(whoami) && cd ${WORKDIR} && ${CRON_SB}") | crontab -
+  (crontab -l; echo "*/10 * * * * pgrep -x \"web\" > /dev/null || ${CRON_SB}") | crontab -
+
 elif [ -e "${WORKDIR}/argo.sh" ]; then
   echo "添加 argo 的 crontab 重启任务"
-  (crontab -l | grep -F "@reboot pkill -kill -u "$(whoami)" && ${CRON_ARGO}") || \
-    (crontab -l; echo "@reboot pkill -kill -u "$(whoami)" && cd ${WORKDIR} && ${CRON_ARGO}") | crontab -
-  (crontab -l | grep -F "pgrep -x \"bot\" > /dev/null || ${CRON_ARGO}") || \
-    (crontab -l; echo "*/10 * * * * pgrep -x \"bot\" > /dev/null || ${CRON_ARGO}") | crontab -
+  (crontab -l; echo "@reboot pkill -kill -u $(whoami) && cd ${WORKDIR} && ${CRON_ARGO}") | crontab -
+  (crontab -l; echo "*/10 * * * * pgrep -x \"bot\" > /dev/null || ${CRON_ARGO}") | crontab -
 fi
