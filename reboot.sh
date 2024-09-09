c9681a8ce51e7a893721b10b45084b92e331f578
yutian81 <165384851+yutian81@users.noreply.github.com>
2024-09-09 13:12:55 +0800

    Update reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+re="\033[0m"
 red="\033[1;91m"
 green="\e[1;32m"
+yellow="\e[1;33m"
+purple="\e[1;35m"
 red() { echo -e "\e[1;91m$1\033[0m"; }
 green() { echo -e "\e[1;32m$1\033[0m"; }
+yellow() { echo -e "\e[1;33m$1\033[0m"; }
+purple() { echo -e "\e[1;35m$1\033[0m"; }
+reading() { read -p "$(red "$1")" "$2"; }
 USERNAME=$(whoami)
 HOSTNAME=$(hostname)
 WORKDIR="/home/${USERNAME}/logs"
@@ -20,18 +26,48 @@ cd "${WORKDIR}" || { echo "无法切换到工作目录 ${WORKDIR}"; exit 1; }
 
 ps aux | grep $(whoami) | grep -v 'sshd\|bash\|grep' | awk '{print $2}' | xargs -r kill -9 > /dev/null 2>&1
 red "已清理所有进程"
+
 nohup ./nezha.sh >/dev/null 2>&1 &
 sleep 2
+if pgrep -x 'npm' > /dev/null; then
+   green "NEZHA 已重启"
+else
+   red "NEZHA 重启失败，尝试再次重启……"
+   pkill -x 'npm' 2>/dev/null && nohup ./nezha.sh >/dev/null 2>&1 &
+   sleep 2
    if pgrep -x 'npm' > /dev/null; then
       green "NEZHA 已重启"
+   else
+      red "NEZHA 重启失败"
    fi
+fi
+
 nohup ./web run -c config.json >/dev/null 2>&1 &
 sleep 2
    if pgrep -x 'web' > /dev/null; then
       green "singbox 已重启"
-   fi
+   else
+      red "singbox 重启失败，尝试再次重启……"
+      pkill -x 'web' && nohup ./web run -c config.json >/dev/null 2>&1 &
+      sleep 2
+      if pgrep -x 'web' > /dev/null; then
+         purple "singbox 已重启"
+      else
+         red "singbox 重启失败"
+      fi
+    fi
+
 nohup ./argo.sh >/dev/null 2>&1 &
 sleep 2
    if pgrep -x 'bot' > /dev/null; then
-      green "ARGO 隧道已重启"
-   fi
+      green "ARGO 已重启"
+   else
+      red "ARGO 重启失败，尝试再次重启……"
+      pkill -x 'bot' && nohup ./argo.sh >/dev/null 2>&1 &
+      sleep 2
+         if pgrep -x 'bot' > /dev/null; then
+	         purple "ARGO 已重启"
+         else
+            red "ARGO 重启失败"
+         fi
+    fi
