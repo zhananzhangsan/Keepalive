05f1057eac7006cfe9bbe6fdc35312e3fbbfbf59
yutian81 <165384851+yutian81@users.noreply.github.com>
2024-09-10 12:52:47 +0800

    Update check_sb_cron.sh

--- check_sb_cron.sh
+++ check_sb_cron.sh
@@ -22,26 +22,36 @@ green "检查并添加 crontab 任务"
 # 先清除旧的相关任务
 (crontab -l | grep -v -E "@reboot pkill -kill -u $(whoami)|pgrep -x \"npm\"|pgrep -x \"web\"|pgrep -x \"bot\"") | crontab -
 
+# 初始化一个 crontab 文件内容
+NEW_CRONTAB=""
+
 # 判断文件是否存在，并根据情况添加任务
 if [ -e "${WORKDIR}/nezha.sh" ] && [ -e "${WORKDIR}/config.json" ] && [ -e "${WORKDIR}/argo.sh" ]; then
   green "添加 nezha & singbox & argo 的 crontab 重启任务"
-  (crontab -l; echo "@reboot pkill -kill -u $(whoami) && ${CRON_NEZHA} ${CRON_SB} ${CRON_ARGO}") | crontab -
-  (crontab -l; echo "*/10 * * * * pgrep -x \"npm\" > /dev/null || ${CRON_NEZHA}") | crontab -
-  (crontab -l; echo "*/10 * * * * pgrep -x \"web\" > /dev/null || ${CRON_SB}") | crontab -
-  (crontab -l; echo "*/10 * * * * pgrep -x \"bot\" > /dev/null || ${CRON_ARGO}") | crontab -
+  chmod +x "${WORKDIR}/npm" && chmod +x "${WORKDIR}/web" && chmod +x "${WORKDIR}/bot"
+  NEW_CRONTAB+="@reboot pkill -kill -u $(whoami) && ${CRON_NEZHA} ${CRON_SB} ${CRON_ARGO}\n"
+  NEW_CRONTAB+="*/10 * * * * pgrep -x \"npm\" > /dev/null || ${CRON_NEZHA}\n"
+  NEW_CRONTAB+="*/10 * * * * pgrep -x \"web\" > /dev/null || ${CRON_SB}\n"
+  NEW_CRONTAB+="*/10 * * * * pgrep -x \"bot\" > /dev/null || ${CRON_ARGO}\n"
 
 elif [ -e "${WORKDIR}/nezha.sh" ]; then
   green "添加 nezha 的 crontab 重启任务"
-  (crontab -l; echo "@reboot pkill -kill -u $(whoami) && cd ${WORKDIR} && ${CRON_NEZHA}") | crontab -
-  (crontab -l; echo "*/10 * * * * pgrep -x \"npm\" > /dev/null || ${CRON_NEZHA}") | crontab -
+  chmod +x "${WORKDIR}/npm"
+  NEW_CRONTAB+="@reboot pkill -kill -u $(whoami) && ${CRON_NEZHA}\n"
+  NEW_CRONTAB+="*/10 * * * * pgrep -x \"npm\" > /dev/null || ${CRON_NEZHA}\n"
 
 elif [ -e "${WORKDIR}/config.json" ]; then
   green "添加 singbox 的 crontab 重启任务"
-  (crontab -l; echo "@reboot pkill -kill -u $(whoami) && cd ${WORKDIR} && ${CRON_SB}") | crontab -
-  (crontab -l; echo "*/10 * * * * pgrep -x \"web\" > /dev/null || ${CRON_SB}") | crontab -
+  chmod +x "${WORKDIR}/web"
+  NEW_CRONTAB+="@reboot pkill -kill -u $(whoami) ${CRON_SB}\n"
+  NEW_CRONTAB+="*/10 * * * * pgrep -x \"web\" > /dev/null || ${CRON_SB}\n"
 
 elif [ -e "${WORKDIR}/argo.sh" ]; then
   green "添加 argo 的 crontab 重启任务"
-  (crontab -l; echo "@reboot pkill -kill -u $(whoami) && cd ${WORKDIR} && ${CRON_ARGO}") | crontab -
-  (crontab -l; echo "*/10 * * * * pgrep -x \"bot\" > /dev/null || ${CRON_ARGO}") | crontab -
+  chmod +x "${WORKDIR}/bot"
+  NEW_CRONTAB+="@reboot pkill -kill -u $(whoami) && ${CRON_ARGO}\n"
+  NEW_CRONTAB+="*/10 * * * * pgrep -x \"bot\" > /dev/null || ${CRON_ARGO}\n"
 fi
+
+# 将 crontab 任务更新一次性添加
+(crontab -l; echo -e "$NEW_CRONTAB") | crontab -
