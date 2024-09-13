8027fb4d836bbe5f8ebdadd5d761612a316301f5
yutian81 <165384851+yutian81@users.noreply.github.com>
2024-09-13 19:15:12 +0800

    Update sb_serv00_socks.sh

--- sb_serv00_socks.sh
+++ sb_serv00_socks.sh
@@ -25,9 +25,11 @@ export NEZHA_PORT=${NEZHA_PORT:-'443'}
 export NEZHA_KEY=${NEZHA_KEY:-''} 
 export ARGO_DOMAIN=${ARGO_DOMAIN:-''}   
 export ARGO_AUTH=${ARGO_AUTH:-''} 
-export VMESS_PORT=${VMESS_PORT:-''}
-export SOCKS_PORT=${SOCKS_PORT:-''}
-export HY2_PORT=${HY2_PORT:-''}
+export VMESS_PORT=${VMESS_PORT:-'40000'}
+export HY2_PORT=${HY2_PORT:-'41000'}
+export SOCKS_PORT=${SOCKS_PORT:-'42000'}
+export SOCKS_USER=${SOCKS_USER:-'abc123'}
+export SOCKS_PASS=${SOCKS_PASS:-'abc123'}
 export CFIP=${CFIP:-'fan.yutian.us.kg'} 
 export CFPORT=${CFPORT:-'443'} 
 
