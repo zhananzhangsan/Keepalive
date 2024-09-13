d9cc1b1f90b22ba30ccb66db68dfa17a9b7a8ca6
yutian81 <165384851+yutian81@users.noreply.github.com>
2024-09-13 18:50:35 +0800

    Update check_sb_cron.sh

--- check_sb_cron.sh
+++ check_sb_cron.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 
-re="\033[0m"
-red="\033[1;91m"
-green="\e[1;32m"
-yellow="\e[1;33m"
-purple="\e[1;35m"
-red() { echo -e "\e[1;91m$1\033[0m"; }
 green() { echo -e "\e[1;32m$1\033[0m"; }
-yellow() { echo -e "\e[1;33m$1\033[0m"; }
-purple() { echo -e "\e[1;35m$1\033[0m"; }
-reading() { read -p "$(red "$1")" "$2"; }
-
 USERNAME=$(whoami)
 WORKDIR="/home/${USERNAME}/logs"
-chmod 777 "${WORKDIR}"
-CRON_NEZHA="nohup ${WORKDIR}/nezha.sh >/dev/null 2>&1 &"
-CRON_SB="nohup ${WORKDIR}/web run -c ${WORKDIR}/config.json >/dev/null 2>&1 &"
-CRON_ARGO="nohup ${WORKDIR}/argo.sh >/dev/null 2>&1 &"
+CRON_NEZHA="nohup ./nezha.sh >/dev/null 2>&1 &"
+CRON_SB="nohup ./web run -c config.json >/dev/null 2>&1 &"
+CRON_ARGO="nohup ./argo.sh >/dev/null 2>&1 &"
 
 # 确保脚本和程序有执行权限
+chmod 777 "${WORKDIR}"
 [ -x "${WORKDIR}/nezha.sh" ] || chmod +x "${WORKDIR}/nezha.sh"
 [ -x "${WORKDIR}/npm" ] || chmod +x "${WORKDIR}/npm"
 [ -x "${WORKDIR}/web" ] || chmod +x "${WORKDIR}/web"
@@ -28,7 +18,7 @@ CRON_ARGO="nohup ${WORKDIR}/argo.sh >/dev/null 2>&1 &"
 
 green "检查并添加 crontab 任务"
 # 先清除旧的相关任务
-(crontab -l | grep -v -E "@reboot pkill -kill -u $(whoami)|pgrep -x \"npm\"|pgrep -x \"web\"|pgrep -x \"bot\"") | crontab -
+(crontab -l | grep -v -E "@reboot pkill -kill -u $(whoami)|pgrep -f \"npm\"|pgrep -x \"web\"|pgrep -x \"bot\"") | crontab -
 
 # 初始化一个 crontab 文件内容
 NEW_CRONTAB=""
@@ -36,25 +26,25 @@ NEW_CRONTAB=""
 # 判断文件是否存在，并根据情况添加任务
 if [ -e "${WORKDIR}/npm" ] && [ -e "${WORKDIR}/web" ] && [ -e "${WORKDIR}/bot" ]; then
   green "添加 nezha & singbox & argo 的 crontab 重启任务"
-  NEW_CRONTAB+="@reboot pkill -kill -u $(whoami) && ${CRON_NEZHA} ${CRON_SB} ${CRON_ARGO}\n"
-  NEW_CRONTAB+="*/10 * * * * pgrep -x \"npm\" > /dev/null || ${CRON_NEZHA}\n"
-  NEW_CRONTAB+="*/10 * * * * pgrep -x \"web\" > /dev/null || ${CRON_SB}\n"
-  NEW_CRONTAB+="*/10 * * * * pgrep -x \"bot\" > /dev/null || ${CRON_ARGO}\n"
+  NEW_CRONTAB+="@reboot pkill -kill -u $(whoami) && cd ${WORKDIR} && ${CRON_NEZHA} ${CRON_SB} ${CRON_ARGO}\n"
+  NEW_CRONTAB+="*/10 * * * * pgrep -x \"npm\" > /dev/null || cd ${WORKDIR} && ${CRON_NEZHA}\n"
+  NEW_CRONTAB+="*/10 * * * * pgrep -x \"web\" > /dev/null || cd ${WORKDIR} && ${CRON_SB}\n"
+  NEW_CRONTAB+="*/10 * * * * pgrep -x \"bot\" > /dev/null || cd ${WORKDIR} && ${CRON_ARGO}\n"
 
 elif [ -e "${WORKDIR}/nezha.sh" ]; then
   green "添加 nezha 的 crontab 重启任务"
-  NEW_CRONTAB+="@reboot pkill -kill -u $(whoami) && ${CRON_NEZHA}\n"
-  NEW_CRONTAB+="*/10 * * * * pgrep -x \"npm\" > /dev/null || ${CRON_NEZHA}\n"
+  NEW_CRONTAB+="@reboot pkill -kill -u $(whoami) && cd ${WORKDIR} && ${CRON_NEZHA}\n"
+  NEW_CRONTAB+="*/10 * * * * pgrep -x \"npm\" > /dev/null || cd ${WORKDIR} && ${CRON_NEZHA}\n"
 
 elif [ -e "${WORKDIR}/config.json" ]; then
   green "添加 singbox 的 crontab 重启任务"
-  NEW_CRONTAB+="@reboot pkill -kill -u $(whoami) ${CRON_SB}\n"
-  NEW_CRONTAB+="*/10 * * * * pgrep -x \"web\" > /dev/null || ${CRON_SB}\n"
+  NEW_CRONTAB+="@reboot pkill -kill -u $(whoami) cd ${WORKDIR} && ${CRON_SB}\n"
+  NEW_CRONTAB+="*/10 * * * * pgrep -x \"web\" > /dev/null || cd ${WORKDIR} && ${CRON_SB}\n"
 
 elif [ -e "${WORKDIR}/argo.sh" ]; then
   green "添加 argo 的 crontab 重启任务"
-  NEW_CRONTAB+="@reboot pkill -kill -u $(whoami) && ${CRON_ARGO}\n"
-  NEW_CRONTAB+="*/10 * * * * pgrep -x \"bot\" > /dev/null || ${CRON_ARGO}\n"
+  NEW_CRONTAB+="@reboot pkill -kill -u $(whoami) && cd ${WORKDIR} && ${CRON_ARGO}\n"
+  NEW_CRONTAB+="*/10 * * * * pgrep -x \"bot\" > /dev/null || cd ${WORKDIR} && ${CRON_ARGO}\n"
 fi
 
 # 将 crontab 任务更新一次性添加
