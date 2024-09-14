bef7b53ff3968195a48d7a924ced5ab27bf61390
yutian81 <165384851+yutian81@users.noreply.github.com>
2024-09-14 14:04:52 +0800

    Update sb00_alive.sh

--- vps_sb00_alive/sb00_alive.sh
+++ vps_sb00_alive/sb00_alive.sh
@@ -26,10 +26,13 @@ export ARGO_DOMAIN=${ARGO_DOMAIN:-''}  # argo隧道域名
 export ARGO_AUTH=${ARGO_AUTH:-''}  # argo隧道token
 export NEZHA_PORT=${NEZHA_PORT:-'5555'}  # 哪吒探针端口，默认5555
 export NEZHA_KEY=${NEZHA_KEY:-''}  # 哪吒探针密钥
+export TERM=xterm
+export DEBIAN_FRONTEND=noninteractive
+export CFIP=${CFIP:-'www.visa.com.tw'}  # 优选域名或优选ip
+export CFPORT=${CFPORT:-'443'}     # 优选域名或优选ip对应端口
 
 # 最大尝试检测次数
 MAX_ATTEMPTS=5
-attempt=0
 # argo 连接状态码
 ARGO_HTTP_CODE=""
 
@@ -51,37 +54,17 @@ install_packages() {
 }
 install_packages
 
-# 拉取远程 json 文件
-curl -s ${VPS_JSON_URL} -o sb00ssh.json
-
-# 读取 JSON 文件并遍历每个服务器的配置
-jq -c '.[]' "sb00ssh.json" | while IFS= read -r server; do
-    HOST=$(echo "$server" | jq -r '.HOST')
-    SSH_USER=$(echo "$server" | jq -r '.SSH_USER')
-    SSH_PASS=$(echo "$server" | jq -r '.SSH_PASS')
-    VMESS_PORT=$(echo "$server" | jq -r '.VMESS_PORT')
-    SOCKS_PORT=$(echo "$server" | jq -r '.SOCKS_PORT')
-    HY2_PORT=$(echo "$server" | jq -r '.HY2_PORT')
-    SOCKS_USER=$(echo "$server" | jq -r '.SOCKS_USER')
-    SOCKS_PASS=$(echo "$server" | jq -r '.SOCKS_PASS')
-    ARGO_DOMAIN=$(echo "$server" | jq -r '.ARGO_DOMAIN')
-    ARGO_AUTH=$(echo "$server" | jq -r '.ARGO_AUTH')
-    NEZHA_SERVER=$(echo "$server" | jq -r '.NEZHA_SERVER')
-    NEZHA_PORT=$(echo "$server" | jq -r '.NEZHA_PORT')
-    NEZHA_KEY=$(echo "$server" | jq -r '.NEZHA_KEY')
-    green "正在处理服务器……服务器地址：$HOST；用户名：$SSH_USER；TCP端口：$VMESS_PORT"
-
 # 判断系统架构，添加对应的定时任务的函数
 add_cron_job() {
     if [ -f /etc/debian_version ] || [ -f /etc/redhat-release ] || [ -f /etc/fedora-release ]; then
-        if crontab -l 2>/dev/null | grep -q "$SCRIPT_PATH"; then
-           red "定时任务已存在，跳过添加计划任务"
+        if ! crontab -l | grep -q "$SCRIPT_PATH" > /dev/null 2>&1; then
+            (crontab -l; echo "*/5 * * * * /bin/bash $SCRIPT_PATH >> /root/00_keep.log 2>&1") | crontab -
+            green "已添加定时任务，每5分钟执行一次"
         else
-           (crontab -l 2>/dev/null; echo "*/5 * * * * /bin/bash $SCRIPT_PATH >> /root/keep.log 2>&1") | crontab -
-           green "已添加定时任务，每5分钟执行一次"
+            red "定时任务已存在，跳过添加计划任务"
         fi
     elif [ -f /etc/alpine-release ]; then
-        local new_cron="*/5 * * * * /bin/bash $SCRIPT_PATH >> /root/keep.log 2>&1"       
+        local new_cron="*/5 * * * * /bin/bash $SCRIPT_PATH >> /root/00_keep.log 2>&1"       
         local current_cron
         if [ -f /var/spool/cron/crontabs/root ]; then
             current_cron=$(cat /var/spool/cron/crontabs/root)
@@ -99,8 +82,28 @@ add_cron_job() {
 }
 add_cron_job
 
+# 拉取远程 json 文件并遍历每个服务器的配置
+curl -s ${VPS_JSON_URL} -o sb00ssh.json
+jq -c '.[]' "sb00ssh.json" | while IFS= read -r servers; do
+    HOST=$(echo "$servers" | jq -r '.HOST')
+    SSH_USER=$(echo "$servers" | jq -r '.SSH_USER')
+    SSH_PASS=$(echo "$servers" | jq -r '.SSH_PASS')
+    VMESS_PORT=$(echo "$servers" | jq -r '.VMESS_PORT')
+    SOCKS_PORT=$(echo "$servers" | jq -r '.SOCKS_PORT')
+    HY2_PORT=$(echo "$servers" | jq -r '.HY2_PORT')
+    SOCKS_USER=$(echo "$servers" | jq -r '.SOCKS_USER')
+    SOCKS_PASS=$(echo "$servers" | jq -r '.SOCKS_PASS')
+    ARGO_DOMAIN=$(echo "$servers" | jq -r '.ARGO_DOMAIN')
+    ARGO_AUTH=$(echo "$servers" | jq -r '.ARGO_AUTH')
+    NEZHA_SERVER=$(echo "$servers" | jq -r '.NEZHA_SERVER')
+    NEZHA_PORT=$(echo "$servers" | jq -r '.NEZHA_PORT')
+    NEZHA_KEY=$(echo "$servers" | jq -r '.NEZHA_KEY')
+    green "正在处理服务器……服务器: $HOST  账户：$SSH_USER  TCP端口：$VMESS_PORT"
+
 # 检测 TCP 端口
-check_vmess_port() {
+check_tcp_port() {
+    local HOST=$1
+    local VMESS_PORT=$2
     nc -zv "$HOST" "$VMESS_PORT" &> /dev/null
     return $?
 }
@@ -122,34 +125,41 @@ run_remote_command() {
     #bash <(curl -Ls ${REBOOT_URL})  #使用此脚本无需重装节点，它将直接启动原本存储在服务器中的命令和配置文件，实现节点重启
 }
 
-# 循环检测
-while [ $attempt -lt $MAX_ATTEMPTS ]; do
-    if ! check_vmess_port; then
-        red "TCP 端口 $VMESS_PORT 不通畅，进程可能不存在，休眠30s后重试"
-        sleep 30
-        attempt=$((attempt+1))
-        continue
-    fi
-    check_argo_status
-    if [ "$ARGO_HTTP_CODE" == "530" ]; then
-        red "Argo 隧道不可用！状态码：$ARGO_HTTP_CODE，进程可能不存在，休眠30s后重试"
-        sleep 30
-        attempt=$((attempt+1))
-        continue
-    fi
-    green "Singbox：TCP端口 $VMESS_PORT 通畅，运行正常；Argo：状态码 $ARGO_HTTP_CODE，运行正常"
-    break
-done
+# 循环遍历服务器检测
+for host in "${!servers[@]}"; do
+    IFS= read -r servers HOST SSH_USER SSH_PASS VMESS_PORT HY2_PORT SOCKS_PORT NEZHA_SERVER NEZHA_PORT NEZHA_KEY ARGO_DOMAIN ARGO_AUTH <<< "${servers[$host]}"
+    attempt=0
+    time=$(TZ="Asia/Hong_Kong" date +"%Y-%m-%d %H:%M")
 
-# 如果达到最大尝试次数，连接服务器并执行远程命令
-if [ $attempt -ge $MAX_ATTEMPTS ]; then
-    red "多次检测失败，尝试通过 SSH 连接并一键安装脚本"
-    if sshpass -p "$SSH_PASS" ssh -o StrictHostKeyChecking=no "$SSH_USER@$HOST" -q exit; then
-        green "SSH远程连接成功!"
-        output=$(run_remote_command)
-        green "正在处理服务器 $HOST 的命令执行结果："
-        echo "$output"
-    else
-        red "SSH 连接失败，请检查你的用户名和密码"
+    while [ $attempt -lt $MAX_ATTEMPTS ]; do
+        if ! check_vmess_port "$HOST" "$VMESS_PORT"; then
+            red "TCP 端口 $VMESS_PORT 不通畅，进程可能不存在，休眠30s后重试"
+            sleep 30
+            attempt=$((attempt+1))
+            continue
+        fi
+        check_argo_status
+        if [ "$ARGO_HTTP_CODE" == "530" ]; then
+            red "Argo 隧道不可用！状态码：$ARGO_HTTP_CODE，进程可能不存在，休眠30s后重试"
+            sleep 30
+            attempt=$((attempt+1))
+            continue
+        fi
+        green "Singbox：TCP端口 $VMESS_PORT 通畅，Argo：状态码 $ARGO_HTTP_CODE，运行正常；\
+        服务器: $HOST 账户：$SSH_USER [$time]"
+        break
+    done
+
+    # 如果达到最大尝试次数，连接服务器并执行远程命令
+    if [ $attempt -ge $MAX_ATTEMPTS ]; then
+        red "多次检测失败，尝试通过SSH连接并远程执行命令, 服务器: $HOST  账户：$SSH_USER  [$time]"
+        if sshpass -p "$SSH_PASS" ssh -o StrictHostKeyChecking=no "$SSH_USER@$HOST" -q exit; then
+            green "SSH远程连接成功, 服务器: $HOST  账户：$SSH_USER  [$time]"
+            output=$(run_remote_command "$HOST" "$SSH_USER" "$SSH_PASS" "$VMESS_PORT" "$HY2_PORT" "$SOCKS_PORT" "$NEZHA_SERVER" "$NEZHA_PORT" "$NEZHA_KEY" "$ARGO_DOMAIN" "$ARGO_AUTH")
+            green "远程命令执行结果："
+            echo "$output"
+        else
+            red "连接失败，请检查你的账户和密码, 服务器: $HOST  账户：$SSH_USER  [$time]"
+        fi
     fi
-fi
+done
