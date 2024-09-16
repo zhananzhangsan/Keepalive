206032564540cc40a0039297d609dda2f97a6682
yutian81 <165384851+yutian81@users.noreply.github.com>
2024-09-16 08:20:42 +0800

    Update reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -6,7 +6,7 @@ USERNAME=$(whoami)
 HOSTNAME=$(hostname)
 WORKDIR="/home/${USERNAME}/logs"
 chmod 777 "${WORKDIR}"
-cd ${WORKDIR} || { echo "无法切换到工作目录 ${WORKDIR}"; exit 1; }
+cd ${WORKDIR} || { red "无法切换到工作目录 ${WORKDIR}"; exit 1; }
 export TMPDIR=$(pwd)
 
 # 确保脚本和程序有执行权限
