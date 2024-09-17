80ed77931534c11a13de27bbf78e080a1a7cb33b
yutian81 <165384851+yutian81@users.noreply.github.com>
2024-09-17 21:47:11 +0800

    Update check_sb_cron.sh

--- check_sb_cron.sh
+++ check_sb_cron.sh
@@ -18,7 +18,7 @@ red "检查已存在的特定任务并清除"
 NEW_CRONTAB=""
 
 # 判断文件是否存在，并根据情况添加任务
-if [ -e "${WORKDIR}/nezha.sh" ] && [ -e "${WORKDIR}/web" ] && [ -e "${WORKDIR}/argo.sh" ]; then
+if [ -e "${WORKDIR}/npm" ] && [ -e "${WORKDIR}/web" ] && [ -e "${WORKDIR}/bot" ]; then
   green "正在添加 nezha & singbox & argo 的 crontab 重启任务"
   NEW_CRONTAB+="@reboot pkill -kill -u $(whoami) && cd ${WORKDIR} && ${CRON_NEZHA} ${CRON_SB} ${CRON_ARGO}\n"
   NEW_CRONTAB+="*/10 * * * * pgrep -x \"npm\" > /dev/null || cd ${WORKDIR} && ${CRON_NEZHA}\n"
