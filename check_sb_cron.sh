f94a5a08565f43b24402c4a996c1d99ad1e87af7
yutian81 <165384851+yutian81@users.noreply.github.com>
2024-09-17 13:19:36 +0800

    Update check_sb_cron.sh

--- check_sb_cron.sh
+++ check_sb_cron.sh
@@ -16,9 +16,9 @@ chmod 777 "${WORKDIR}"
 [ -x "${WORKDIR}/argo.sh" ] || chmod +x "${WORKDIR}/argo.sh"
 [ -x "${WORKDIR}/bot" ] || chmod +x "${WORKDIR}/bot"
 
-green "检查已存在的特定任务并清除"
+red "检查已存在的特定任务并清除"
 (crontab -l | grep -v -E "@reboot pkill -kill -u $(whoami)|pgrep -x \"npm\"|pgrep -x \"web\"|pgrep -x \"bot\"") | crontab -
-#green "清除所有已存在的 crontab 任务"
+#red "清除所有已存在的 crontab 任务"
 #crontab -r
 
 # 初始化一个新的 crontab 文件内容
@@ -26,24 +26,24 @@ NEW_CRONTAB=""
 
 # 判断文件是否存在，并根据情况添加任务
 if [ -e "${WORKDIR}/npm" ] && [ -e "${WORKDIR}/web" ] && [ -e "${WORKDIR}/bot" ]; then
-  green "添加 nezha & singbox & argo 的 crontab 重启任务"
+  green "正在添加 nezha & singbox & argo 的 crontab 重启任务"
   NEW_CRONTAB+="@reboot pkill -kill -u $(whoami) && cd ${WORKDIR} && ${CRON_NEZHA} ${CRON_SB} ${CRON_ARGO}\n"
   NEW_CRONTAB+="*/10 * * * * pgrep -x \"npm\" > /dev/null || cd ${WORKDIR} && ${CRON_NEZHA}\n"
   NEW_CRONTAB+="*/10 * * * * pgrep -x \"web\" > /dev/null || cd ${WORKDIR} && ${CRON_SB}\n"
   NEW_CRONTAB+="*/10 * * * * pgrep -x \"bot\" > /dev/null || cd ${WORKDIR} && ${CRON_ARGO}\n"
 
 elif [ -e "${WORKDIR}/nezha.sh" ]; then
-  green "添加 nezha 的 crontab 重启任务"
+  green "正在添加 nezha 的 crontab 重启任务"
   NEW_CRONTAB+="@reboot pkill -kill -u $(whoami) && cd ${WORKDIR} && ${CRON_NEZHA}\n"
   NEW_CRONTAB+="*/10 * * * * pgrep -x \"npm\" > /dev/null || cd ${WORKDIR} && ${CRON_NEZHA}\n"
 
 elif [ -e "${WORKDIR}/config.json" ]; then
-  green "添加 singbox 的 crontab 重启任务"
+  green "正在添加 singbox 的 crontab 重启任务"
   NEW_CRONTAB+="@reboot pkill -kill -u $(whoami) cd ${WORKDIR} && ${CRON_SB}\n"
   NEW_CRONTAB+="*/10 * * * * pgrep -x \"web\" > /dev/null || cd ${WORKDIR} && ${CRON_SB}\n"
 
 elif [ -e "${WORKDIR}/argo.sh" ]; then
-  green "添加 argo 的 crontab 重启任务"
+  green "正在添加 argo 的 crontab 重启任务"
   NEW_CRONTAB+="@reboot pkill -kill -u $(whoami) && cd ${WORKDIR} && ${CRON_ARGO}\n"
   NEW_CRONTAB+="*/10 * * * * pgrep -x \"bot\" > /dev/null || cd ${WORKDIR} && ${CRON_ARGO}\n"
 fi
