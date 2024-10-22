ada9bdad2358541b01bb1271822fac76f2f6e667
yutian81 <165384851+yutian81@users.noreply.github.com>
2024-10-22 12:42:25 +0800

    Update sb00_alive.sh

--- vps_sb00_alive/sb00_alive.sh
+++ vps_sb00_alive/sb00_alive.sh
@@ -221,8 +221,8 @@ process_servers() {
             
             # 如果所有检查都通过，则打印通畅信息并退出循环
             if [ "$all_checks" == true ]; then
-                green "TCP 端口 $(yellow "$VMESS_PORT") 通畅; Argo $(yellow "$ARGO_DOMAIN") 正常; 哪吒探针 $(yellow "$server_name") 正常 \
-                服务器 $(yellow "$HOST") 一切正常！ 账户：$(yellow "$SSH_USER")  [$time]"
+                green "TCP 端口 $(yellow "$VMESS_PORT") 通畅; Argo $(yellow "$ARGO_DOMAIN") 正常; 哪吒探针 $(yellow "$server_name") 正常"
+                green "服务器 $(yellow "$HOST") 一切正常！ 账户：$(yellow "$SSH_USER")  [$time]"
                 break
             fi
         done
